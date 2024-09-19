fe8334d09dcb486e12ff28ad0620bc21068124fa
JH CHO <junh9541@gmail.com>
2024-09-19 17:57:08 +0900

    feat: db 엔티티 수정, 리스트 컬럼 추가

--- Back/src/main/java/com/example/Back/entity/Diary.java
+++ Back/src/main/java/com/example/Back/entity/Diary.java
@@ -1,42 +1,64 @@
 package com.example.Back.entity;
 
-import java.sql.Date;
+import java.util.Date;
+import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import com.example.Back.repository.DiaryRepository;
+import com.example.Back.repository.converter.EmotionFieldConverter;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @Table(name="diary")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Diary {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@ManyToOne(cascade = CascadeType.REMOVE)
 	@JoinColumn(name = "user_id", updatable = false)
 	private Member member;
-	
+
 	@Column(name="date")
+	@Temporal(TemporalType.DATE)
+	@CreatedDate
 	private Date date;
-	
+
 	@Column(name="emotion_field")
-	private String[] emotionField;
-	
+	@Convert(converter = EmotionFieldConverter.class)
+	private List<String> emotionField;
+
 	@Column(name="music_title")
 	private String musicTitle;
-	
+
 	@Column(name="music_id")
 	private String musicId;
-	
-	
+
+	@Column(name = "content")
+	private String content;
+
+	@Column(name = "praise")
+	private String praise;
+
+	@Builder
+	public Diary(Member member, List<String> emotionField, String musicTitle,
+				 String musicId, String content, String praise) {
+		this.member = member;
+		this.emotionField = emotionField;
+		this.musicTitle = musicTitle;
+		this.musicId = musicId;
+		this.content = content;
+		this.praise = praise;
+	}
+
+
 	
 }
